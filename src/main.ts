8feb1b59a206f59ada4dd001ba6f15076678df09
SophieDeGeorge <sophiedegeorge142@gmail.com>
2025-10-13 02:24:43 +0000

    Step 2 complete, working counter and label

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,26 @@
 import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
 import "./style.css";
 
+let counter: number = 0;
+
 document.body.innerHTML = `
-  <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
-  <!-- <p>HI I'M AEGIS HELLO : -->
-  <button class="dog button" type="button">🐕</button>
+                                  <!-- <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p> -->
+                                  <!-- <p>HI I'M AEGIS HELLO : -->
+  <p>Counter: <span id="counter">0</span></p>
+  <button id="dog button">🐕</button>
+
 </p>`;
+
+//Click handler
+const button = document.getElementById("dog button")!;
+const counterElement = document.getElementById("counter")!;
+counterElement.innerHTML = counter.toString() + " dogs";
+
+button.addEventListener("click", () => {
+  counter++;
+  counterElement.innerHTML = counter.toString() + " dogs";
+  console.log("Num: ", button, counterElement, counter);
+});
 
 console.log("hi im aegis jr");
 console.log("hi im aegis sr");
