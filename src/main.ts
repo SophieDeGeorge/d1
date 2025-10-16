f9e9015e6b5f545a0f465826b89af4c9479a8548
SophieDeGeorge <sophiedegeorge142@gmail.com>
2025-10-16 02:13:09 +0000

    Step 4.5, upgrade button implemented but not working

--- src/main.ts
+++ src/main.ts
@@ -3,36 +3,84 @@ import "./style.css";
 
 let counter: number = 0;
 let zero = 0;
+let timeBetweenFrames = 0;
+let rateUpgrades = 0;
+let incrementRate = 0;
+const oneSecond = 1000;
 
 document.body.innerHTML = `
                                   <!-- <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p> -->
                                   <!-- <p>HI I'M AEGIS HELLO : -->
   <p>Counter: <span id="counter">0</span></p>
   <button id="dog button">🐕</button>
+  <button id="rateUp" command="disabled">Upgrade Cost: 10 dogs</button>
 
 </p>`;
 
-//Click handler
-const button = document.getElementById("dog button")!;
+//Dog Button Click handler
+const dogButton = document.getElementById("dog button")!;
 const counterElement = document.getElementById("counter")!;
 counterElement.innerHTML = counter.toString() + " dogs";
 
-button.addEventListener("click", () => {
-  counter++;
+//Dog Button Event Listener
+dogButton.addEventListener("click", () => {
+  increaseCounterBy(1);
   counterElement.innerHTML = counter.toString() + " dogs";
-  console.log("Num: ", button, counterElement, counter);
+  //console.log("Num: ", dogButton, counterElement, counter);
 });
 
+//rateUp Button Click handler
+const rateUpButton = <HTMLInputElement> document.getElementById("rateUp")!;
+rateUpButton.disabled = true;
+
+//rateUp Event listener
+rateUpButton.addEventListener("click", () => {
+  rateUpgrades += 1;
+  counter -= 10;
+  increaseCounterBy(0);
+});
+
+function updateDogAmount() {
+  counterElement.innerHTML = counter.toString() + " dogs";
+}
+
+function checkDogAmount() {
+  if (counter < 10) {
+    console.log("Disabled");
+    rateUpButton.disabled = true;
+  } else {
+    console.log("Enabled");
+    rateUpButton.disabled = false;
+  }
+}
+
+function increaseCounterBy(x: number) {
+  counter += x;
+  updateDogAmount();
+  checkDogAmount();
+  fractionalIncrement();
+}
+
 // Increment fractionally
-requestAnimationFrame(firstFrame);
+function fractionalIncrement() {
+  if (rateUpgrades > 0) {
+    requestAnimationFrame(firstFrame);
+  }
+}
+
 function firstFrame(timestamp: number) {
   zero = timestamp;
   incrementCounter(timestamp);
 }
 
 function incrementCounter(timestamp: number) {
-  counter += (timestamp - zero) / 1000;
-                //console.log(timestamp - zero);
+  timeBetweenFrames = timestamp - zero;
+  incrementRate = timeBetweenFrames / (oneSecond / rateUpgrades); //divide the miliseconds between frames by the result of 1000ms / rateUpgrades
+  increaseCounterBy(incrementRate);
+  //counter += incrementRate;
+
+  //console.log(timestamp - zero);
+
   counterElement.innerHTML = counter.toString() + " dogs";
   zero = timestamp;
   requestAnimationFrame(incrementCounter);
